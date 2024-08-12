44d4c17ecaa78de4cba7f9b2500d58f620cfcd07
daniyalumer <daniyalharal100@gmail.com>
2024-08-12 15:27:04 +0500

    First part of project completed
    -Downloading image using wget
    -Corrected Argument Parsing
    -Resolved Errors related to Image not downloading and saving

--- bash_project.sh
+++ bash_project.sh
@@ -1,10 +1,13 @@
-#default values
+#!/bin/bash
+
+# Default values
 save_path="."
 resolution="1920x1080"
 file_name="wallpaper"
+temp_dir="/tmp"
 
 function print_usage(){
-	echo "Usage: $0 [ -p PATH | -t ] [ -o FILENAME ] [ -r RESOLUTION ] [ -d ]"
+    echo "Usage: $0 [ -p PATH | -t ] [ -o FILENAME ] [ -r RESOLUTION ] [ -d ]"
     echo
     echo "        -p PATH       Save image to the specified PATH."
     echo "        -t            Save image to /tmp directory."
@@ -17,54 +20,66 @@ function print_usage(){
 }
 
 function convert_resolution(){
-	result=$1
-	echo "${result//x/\/}"
+    result=$1
+    echo "${result//x/\/}"
 }
 
-while getopt p:o:r:td opt; do
-	case ${opt} in
-		p )
-			save_path=$OPTARG
-			;;
-		o )
-			file_name=$OPTARG
-			;;
-		r )
-			resolution=$(covert_resolution $OPTARG)
-			;;
-		t )
-			save_path=$temp_dir
-		h )
-			print_usage
-			exit 1
-			;;
-		\? )
-			print_usage
-			exit 1
-			;;
-		: )
-			echo "Invalid option: -$OPTARG requires an argument" 1>&2
-			print_usage
-			exit 1
-			;;
-	esac
+while getopts p:o:r:td opt; do
+    case ${opt} in
+        p )
+            save_path=$OPTARG
+            ;;
+        o )
+            file_name=$OPTARG
+            ;;
+        r )
+            resolution=$(convert_resolution $OPTARG)
+            ;;
+        t )
+            save_path=$temp_dir
+            ;;
+        d )
+            # Default settings can be adjusted here
+            ;;
+        h )
+            print_usage
+            exit 1
+            ;;
+        \? )
+            print_usage
+            exit 1
+            ;;
+        : )
+            echo "Invalid option: -$OPTARG requires an argument" 1>&2
+            print_usage
+            exit 1
+            ;;
+    esac
 done
 
 shift $((OPTIND - 1))
 
-# Now $1 refers to the first non-option argument
-echo "Remaining arguments: $@"
+# Debug output
+echo "Save path: $save_path"
+echo "File name: $file_name"
+echo "Resolution: $resolution"
 
-url="https://picsum.photos/$resolution
+# Construct URL and output path
+url="https://picsum.photos/$resolution"
+output_path="$save_path/$file_name.jpg"
 
-output_path="$file_path/$file_name.jpg"
+echo "URL: $url"
+echo "Output path: $output_path"
 
-echo "Downloading image to $filepath"
+# Make sure the directory exists
+mkdir -p "$save_path"
 
-curl -o "$output_path" "$url"
+# Download the image
+wget -O "$output_path" "$url"
 
-echo "Image Downloaded Successfully"
-
-
-
-
+# Check if the file was downloaded
+if [ -f "$output_path" ]; then
+    echo "Image Downloaded Successfully"
+else
+    echo "Failed to download image" 1>&2
+fi
